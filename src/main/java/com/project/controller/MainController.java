bdbb034cbb281dd663129e7d47b9c89cdd80ec9c
seokho <notdolseok@naver.com>
2023-08-21 10:51:34 +0900

    mainMap

--- src/main/java/com/project/controller/MainController.java
+++ src/main/java/com/project/controller/MainController.java
@@ -1,31 +1,15 @@
 package com.project.controller;
 
-import java.util.HashMap;
-import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.project.dto.UserDTO;
-import com.project.service.UserService;
-
-import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class MainController {
-	@RequestMapping("/kakao")
+	@RequestMapping("/mainmap")
 	public String kakaoMap() {
-		return "kakaoMap";
-	}
-
-	@RequestMapping("/useparking")
-	public String useparking() {
-		return "useparking";
-	}
-	
-	@RequestMapping("/pay")
-	public String pay() {
-		return "pay";
+		return "mainmap/mainmap";
 	}
 
 	@RequestMapping("/header")
